ae1e23a3ed6d533b8c0ce87b7fefb8ac7713c166
Luis Howell <luisbhowell@gmail.com>
2019-02-06 12:22:55 +0000

    added gitignore and updated obd2 header

--- Software/OBD2/obd2.hpp
+++ Software/OBD2/obd2.hpp
@@ -21,11 +21,11 @@ class obd2{
         float decoded_cmd(string cmd);
         vector<string> read_dtc();
         string dtc_desc(string dtc_code);
+        int hex2int(string hex_string);
+        string int2hex(int int_value);
     
     private:
 
-        int hex2int(string hex_string);
-        string int2hex(int int_value);
         int setup_obd(string comm_port);   
 
 };
