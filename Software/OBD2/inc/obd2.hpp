e8c3f8cb1202044423158961814f7d2e086e22c0
Luis Howell <luisbhowell@gmail.com>
2019-02-17 18:34:46 +0000

    added doxygen comments. removed data size check

--- Software/OBD2/inc/obd2.hpp
+++ Software/OBD2/inc/obd2.hpp
@@ -1,3 +1,11 @@
+/**
+* @file obd2.hpp
+* @brief The class obd2 provides methods for communicating with an OBD2 vhicle over Bluetooth.
+*
+* @author Luis Howell
+*
+*/
+
 #ifndef OBD2_H
 #define OBD2_H
 
@@ -12,16 +20,84 @@ class obd2{
         bool connected = false;
         vector<string> supported_pids;
 
+        /**
+         * @brief obd2 object constructor
+         * 
+         * @param comm_port The device location of the bluetooth serial port, often /dev/rfcomm0
+         */
         obd2(string comm_port);
 
+
+        /**
+         * @brief Send a command to the OBD2 module
+         * 
+         * @param cmd The command to send in the format of a hex string
+         * @param parse A bool flag which sets whether the response should be parsed to remove all non-data in the response e.g spaces, new lines
+         * @return string The reponse from the OBD2 module
+         */
         string send_cmd(string cmd, bool parse = false);
+
+        /**
+         * @brief Scans the OBD2 PIDs supported by the vehcile
+         * 
+         * Stores the supported PIDs as a vector of strings in supported_pids 
+         * 
+         */
         void scan_pids();
+
+        /**
+         * @brief Prints the supported PIDs and their descriptions
+         * 
+         * scan_pids() must have been ran at least once
+         */
         void print_supported_pids();
+
+        /**
+         * @brief Decodes the Hex responses from the OBD2 module
+         * 
+         * @param response The hex response 
+         * @param option A few PIDs have multiple outputs, option selects which one will be returned
+         * @return float The decoded value
+         */
         float decode_response(string response, int option = 1);
+
+        /**
+         * @brief Sends a command and decodes the response
+         * 
+         * @param cmd The command to send as a hex string
+         * @return float The decoded hex response
+         */
         float decoded_cmd(string cmd);
+
+        /**
+         * @brief Reads the currently stored diagnostic trouble codes (DTC)
+         * 
+         * @return vector<string> A vector of strings containing the DTCs
+         */
         vector<string> read_dtc();
+
+        /**
+         * @brief Reads the DTC description
+         * 
+         * @param dtc_code A DTC code
+         * @return string The desctiption of the DTC code
+         */
         string dtc_desc(string dtc_code);
+
+        /**
+         * @brief Converts a hex string to an integer
+         * 
+         * @param hex_string A hex string
+         * @return unsigned long The int value of the hex string
+         */
         unsigned long hex2int(string hex_string);
+
+        /**
+         * @brief Converts an integer to a hex string
+         * 
+         * @param int_value An integer value
+         * @return string A hex string representation of the integer
+         */
         string int2hex(long int_value);
     
     private:
