250dc4a36dc008bcf75bef3b8dc7cf4bd3191bcc
Joe Walker <joew2410@gmail.com>
2019-03-28 17:00:37 +0000

    added defines for registers

--- Software/Sensors/src/MMA8652FCR1.cpp
+++ Software/Sensors/src/MMA8652FCR1.cpp
@@ -34,25 +34,25 @@ bool MMA8652FCR1::init(int devID, int intPin1, int intPin2)
 
 
     //check the who am i register
-    int result = wiringPiI2CReadReg8(this->fd, 0x0D);
+    int result = wiringPiI2CReadReg8(this->fd, MMA8652FCR1_WHO_AM_I_REG);
     cout << "Who am i reg says: 0x"  << hex << result << endl;
-    if(result != 0x4A)
+    if(result != MMA8652FCR1_DEVICE_ID)
     {
         cout << "Who am i register mistmatch." << endl << "Expecting: 0x4A Received: " << hex << result << endl; 
     }
     
     //set the configuration registers
-    result = wiringPiI2CWriteReg8(this->fd, 0x2A, 0x00); //put into standby mode
+    result = wiringPiI2CWriteReg8(this->fd, MMA8652FCR1_CTRL_REG1, 0x00); //put into standby mode
     
-    result = wiringPiI2CWriteReg8(this->fd, 0x09, 0x00); //turn off the FIFO
-    result = wiringPiI2CWriteReg8(this->fd, 0x0E, 0x02); //set the range to 8G
-    result = wiringPiI2CWriteReg8(this->fd, 0x2A, 0x18); //set data rate to 100Hz
-    result = wiringPiI2CWriteReg8(this->fd, 0x2B, 0x00); //set power mode to auto sleep off
-    result = wiringPiI2CWriteReg8(this->fd, 0x2C, 0x00); //set interrupt to be active high
-    result = wiringPiI2CWriteReg8(this->fd, 0x2D, 0x01); //set the interrupt to be triggered by data ready
-    result = wiringPiI2CWriteReg8(this->fd, 0x2E, 0x01); //route interrupt to INT2 pin
+    result = wiringPiI2CWriteReg8(this->fd, MMA8652FCR1_F_SETUP_REG, 0x00); //turn off the FIFO
+    result = wiringPiI2CWriteReg8(this->fd, MMA8652FCR1_XYZ_DATA_CFG_REG, 0x02); //set the range to 8G
+    result = wiringPiI2CWriteReg8(this->fd, MMA8652FCR1_CTRL_REG1, 0x18); //set data rate to 100Hz
+    result = wiringPiI2CWriteReg8(this->fd, MMA8652FCR1_CTRL_REG2, 0x00); //set power mode to auto sleep off
+    result = wiringPiI2CWriteReg8(this->fd, MMA8652FCR1_CTRL_REG3, 0x00); //set interrupt to be active high
+    result = wiringPiI2CWriteReg8(this->fd, MMA8652FCR1_CTRL_REG4, 0x01); //set the interrupt to be triggered by data ready
+    result = wiringPiI2CWriteReg8(this->fd, MMA8652FCR1_CTRL_REG5, 0x01); //route interrupt to INT2 pin
     
-    result = wiringPiI2CWriteReg8(this->fd, 0x2A, 0x19); //set to active mode
+    result = wiringPiI2CWriteReg8(this->fd, MMA8652FCR1_CTRL_REG1, 0x19); //set to active mode
     
     //get static values
     staticVals = new accValues_t;
@@ -83,12 +83,12 @@ void MMA8652FCR1::getData(accValues_t *data)
 #ifdef RPI
 
     //read the data values from the registers
-    int xMSB = wiringPiI2CReadReg8(this->fd,0x01);
-    int xLSB = wiringPiI2CReadReg8(this->fd,0x02);
-    int yMSB = wiringPiI2CReadReg8(this->fd,0x03);
-    int yLSB = wiringPiI2CReadReg8(this->fd,0x04);
-    int zMSB = wiringPiI2CReadReg8(this->fd,0x05);
-    int zLSB = wiringPiI2CReadReg8(this->fd,0x06);
+    int xMSB = wiringPiI2CReadReg8(this->fd,MMA8652FCR1_OUT_X_MSB_REG);
+    int xLSB = wiringPiI2CReadReg8(this->fd,MMA8652FCR1_OUT_X_LSB_REG);
+    int yMSB = wiringPiI2CReadReg8(this->fd,MMA8652FCR1_OUT_Y_MSB_REG);
+    int yLSB = wiringPiI2CReadReg8(this->fd,MMA8652FCR1_OUT_Y_LSB_REG);
+    int zMSB = wiringPiI2CReadReg8(this->fd,MMA8652FCR1_OUT_Z_MSB_REG);
+    int zLSB = wiringPiI2CReadReg8(this->fd,MMA8652FCR1_OUT_Z_LSB_REG);
     
     
     //do twos complement conversion
@@ -148,7 +148,7 @@ double MMA8652FCR1::twosComp(uint8_t MSB, uint8_t LSB)
     }
         
     //scale to 8G
-    returnVal = ((twosCompVal & 0xFF) * 8 * sign)/(pow(2,11));
+    returnVal = ((twosCompVal & 0xFFF) * 8 * sign)/(pow(2,11));
     
     return returnVal;
 }
